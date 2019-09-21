c3509f1c5c4e0d6c502487518f5fd3377708ad95
kunal <kunal1998doon@gmail.com>
2019-09-21 16:25:02 +0530

    added script to cut the ropes

---
+++ Assets/Scripts/Cut.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cut : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            RaycastHit2D ray = Physics2D.Raycast(this.GetComponent<Camera>().ScreenToWorldPoint(Input.mousePosition),Vector2.zero);
+            if (ray.collider != null)
+            {
+                if (ray.collider.tag == "Rope")
+                {
+                    Destroy(ray.collider.gameObject);
+                }
+            }
+        }
+    }
+}
